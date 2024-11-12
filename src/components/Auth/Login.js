8b05886a77ba6ad48909604e3fefba10df300e54
Rafael <rafaelmbeleboni@gmail.com>
2024-11-11 21:41:27 -0300

    rota login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     console.log('Submitting login with:', { email, password });
 
     try {
-      const response = await axios.post('http://localhost:5001/api/User/Login', {
+      const response = await axios.post('http://pj2-biblioteca-univesp.onrender.com:80/api/User/Login', {
         email,
         password,
       });
